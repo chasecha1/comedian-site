fe5825af3304ada6723b77fae87c7d2084a51371
chasecha1 <yht7nf@virginia.edu>
2025-04-07 15:57:17 -0400

    progress(comedian filtering): added very basic UI for filtering comedians on keywords - will have to change to modal UI in future but POC for now.

--- src/components/ComedianList.tsx
+++ src/components/ComedianList.tsx
@@ -1,6 +1,8 @@
+'use client'
 import { ComedianProfile } from "../../types/entities"
 import Image from "next/image";
 import { urlFor } from '@/sanity/sanityImage';
+import { useState } from "react";
 import { PortableText } from "next-sanity";
 
 
@@ -10,16 +12,53 @@ type ComedianProps = {
 }
 
 const ComedianList = ({comedians} :ComedianProps) => {
+  const [selectedKeywords, setSelectedKeywords] = useState<string[]>([]);
   const comedianList = comedians
+
+  const allKeywords = Array.from(new Set(
+    comedianList.flatMap(comedian => comedian.keywords)
+  ));
+
+  const toggleKeyword = (keyword: string) => {
+    setSelectedKeywords(prev =>
+      prev.includes(keyword)
+        ? prev.filter(k => k !== keyword)
+        : [...prev, keyword]
+    );
+  };
+
+  const filteredComedians =
+  selectedKeywords.length > 0
+    ? comedians.filter(comedian =>
+        comedian.keywords.some(keyword =>
+          selectedKeywords.includes(keyword)
+        )
+      )
+    : comedians;
 
   return (
     <div className="ComedianList bg-white dark:bg-light-navy flex flex-col justify-center items-center">
       <div className="Title Heading--head text-navy-text dark:text-white pt-8 pb-4">
           Comedians
       </div>
+      <div className="flex flex-wrap justify-center gap-2 mb-4">
+        {allKeywords.map((keyword) => (
+          <button
+            key={keyword}
+            onClick={() => toggleKeyword(keyword)}
+            className={`px-4 py-2 rounded-full border transition ${
+              selectedKeywords.includes(keyword)
+                ? 'bg-navy text-white border-navy'
+                : 'bg-light-navy dark:bg-white text-white dark:text-deep-navy border-navy hover:bg-navy hover:text-white'
+            }`}
+          >
+            {keyword}
+          </button>
+        ))}
+      </div>
       <div className="Card-containers flex flex-col lg:flex-row justify-center pb-8 px-2 lg:px-8 text-navy-text dark:text-white">
-        {comedianList && comedianList.map((comedian: ComedianProfile) => (
-            <div className="Comedian-Card shadow-brand-box dark:shadow-brand-box-navy pb-4 lg:pb-0 mb-8 lg:mb-2 mx-4 lg:w-1/3" key={comedian._id}>
+        {comedianList && filteredComedians.map((comedian: ComedianProfile) => (
+            <div className="Comedian-Card shadow-brand-box dark:shadow-brand-box-navy pb-4 lg:pb-0 mb-8 lg:mb-2 mx-4" key={comedian._id}>
               <div className="Media pb-4 flex justify-center">
                 <Image
                   src={urlFor(comedian.image).width(480).height(430).fit('crop').crop('top').url()}
@@ -33,7 +72,12 @@ const ComedianList = ({comedians} :ComedianProps) => {
                   <p>{comedian.name}</p>
                 </div>
                 <div className="description mx-8 pb-4">
-                  <PortableText value={comedian.bio}/>
+                  {/* <PortableText value={comedian.bio}/> */}
+                  {comedian.keywords.map((keyword: string, index) => (
+                    <div key={index}>
+                      {keyword}
+                    </div>
+                  ))}
                 </div>
               </div>
             </div>
